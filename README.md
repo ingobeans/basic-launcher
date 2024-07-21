ac4c0e4825796af4705635d27de3c34ebd104ecb
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-07-21 17:48:56 +0200

    added config validation

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 # Running
 
 1. (optional) Set up virtual environment. This makes building easier. `python3 -m venv venv`
-2. Install packages. `pip install eel pyinstaller vdf`
+2. Install packages. `pip install eel pyinstaller vdf json_repair`
 3. Run main. `python3 main.py`
--- config.py
+++ config.py
@@ -34,6 +34,14 @@ def get_default_config():
         }
     }
 
+def merge_dicts(a, b):
+    for key, value in b.items():
+        if isinstance(value, dict) and key in a:
+            a[key] = merge_dicts(a.get(key, {}), value)
+        else:
+            a[key] = value
+    return a
+
 def save_config():
     with open(os.path.join(get_app_data_directory(), "settings.json"), "w", encoding="utf-8") as f:
         json.dump(active_config, f)
@@ -42,6 +50,29 @@ def load_config():
     with open(os.path.join(get_app_data_directory(), "settings.json"), "r", encoding="utf-8") as f:
         return json.load(f)
 
+def verify_config():
+    # function to verify and fix the config file
+    # this includes fixing invalid json and adding missing keys
+    with open(os.path.join(get_app_data_directory(), "settings.json"), "r", encoding="utf-8") as f:
+        data = f.read()
+    if not data:
+        data = "{}"
+
+    try:
+        j = json.loads(data)
+    except ValueError:
+        # config json is invalid
+        import json_repair
+        j = json_repair.repair_json(data, return_objects=True)
+
+    # make sure no keys included in the default config are missing
+    default = get_default_config()
+    new = merge_dicts(default, j)
+    if new != data:
+        print("current config is bad")
+        with open(os.path.join(get_app_data_directory(), "settings.json"), "w", encoding="utf-8") as f:
+            json.dump(new, f)
+
 if not os.path.exists(get_app_data_directory()):
     os.mkdir(get_app_data_directory())
     active_config = get_default_config()
@@ -49,5 +80,6 @@ if not os.path.exists(get_app_data_directory()):
     print(f"created {get_app_data_directory()}")
 else:
     print(f"data dir {get_app_data_directory()}")
+    verify_config()
     active_config = load_config()
 print(active_config)
